bb099e21200e2a991a3adcb20733ae09b311a814
092FilichevAA <fefa12@yandex.ru>
2020-09-21 17:54:31 +0300

    Перебор по типам ячейки

--- src/main/java/ru/TableChanger/TableChanger.java
+++ src/main/java/ru/TableChanger/TableChanger.java
@@ -6,16 +6,14 @@ package ru.TableChanger;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Iterator;
 
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.FormulaEvaluator;
 import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.xssf.usermodel.XSSFCell;
-import org.apache.poi.xssf.usermodel.XSSFRow;
 
 
 public class TableChanger {
@@ -24,28 +22,59 @@ public class TableChanger {
     public static void main(String[] args) throws IOException {
     HSSFSheet book;
         int n = 1;
+        String test1 = null;
+        String test2 = "";
+        String test3 = "";
 
-            // Read XSL file
+            // Читаем файл
             FileInputStream inputStream = new FileInputStream(new File("F:/1.xls"));
 
-            // Get the workbook instance for XLS file
+            // Берем воркбук
             HSSFWorkbook workbook = new HSSFWorkbook(inputStream);
 
-            // Get first sheet from the workbook
+            // Первая страница воркбука
             HSSFSheet sheet = workbook.getSheetAt(0);
 
-            // Get iterator to all the rows in current sheet
+            // Перебираем строки а в них ячейки.
             Iterator<Row> it = sheet.iterator();
             while (it.hasNext()) {
                 Row row = it.next();
                 Iterator<Cell> cells = row.iterator();
                 while (cells.hasNext()) {
                     Cell cell = cells.next();
-                    System.out.println("проход " +n);
-                    n++;
+                    {
+                        if(cell.getCellType() == Cell.CELL_TYPE_STRING)
+                        {
+                            test1 = cell.getStringCellValue();
+
+                            if (test1 == Resthings.NAMEPFR_etalon){
+                                cell.setCellValue(Resthings.NAMEPFR);
+                                System.out.println("ЗАмена на ходу №  " +n);
+                            }
+                        }
+                        if(cell.getCellType() == Cell.CELL_TYPE_NUMERIC)
+                        {
+
+                        }
+                        int cellType = cell.getCellType();
+
+                        //System.out.println("проход " +n);
+
+                        n++;
+                    }
+
                 }
             }
-        System.out.println("");
+        System.out.println(" ");
+
+        //         создаем подписи к столбцам (это будет первая строчка в листе Excel файла)
+        //         записываем созданный в памяти Excel документ в файл
+                try (FileOutputStream out = new FileOutputStream(new File("F:/2.xls"))) {
+                    workbook.write(out);
+                    System.out.println("Файл complite");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
     }
 
   //  Sev@$0921232
